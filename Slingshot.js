e4e733a96f635d2a12d3a8ed4399f95723221fa7
Prithvi1122 <prithviprem2006@gmail.com>
2020-06-18 17:54:30 +0530

    on 18 June

--- Slingshot.js
+++ Slingshot.js
@@ -1,24 +1,49 @@
-class Slingshot{
-    constructor(object1,pointB){
-        var options={
-            bodyA:object1,
-            pointB:pointB,
-            stiffness:0.04,
-            length:10
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
         }
-        this.pointB=pointB
-        this.sling=Constraint.create(options);
-        World.add(world,this.sling);
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        this.image = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png");
+        this.image3 = loadImage("sprites/sling3.png");
+        World.add(world, this.sling);
     }
-    display(){
-        if(this.sling.bodyA){
-            strokeWeight(4);
-            var pointA=this.sling.bodyA.position;
-            var pointB=this.pointB;
-            line(pointA.x,pointA.y,pointB.x,pointB.y) 
-        }
-    }
+
     fly(){
-        this.sling.bodyA=null;
+        this.sling.bodyA = null;
     }
+
+    display(){
+        
+        image(this.image,200,30);
+        image(this.image2,170,25);
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+            if(pointA.x<220){
+                image(this.image3,pointA.x-20,pointA.y-10,15,30);
+                strokeWeight(7);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+            }
+            else{
+                image(this.image3,pointA.x-20,pointA.y-10,15,30);
+                strokeWeight(3);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+            }
+            
+            pop();
+        }
+    }
+    
 }
--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform,constraint_log;
-var chain;
+var backgroundImg,platform;
+var bird, slingShot;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -34,18 +36,17 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
-    constraint_log=new Log(230,180,80,90);
-    sling=new Slingshot(bird.body,{x:200,y:100});
-   
+    bird = new Bird(100,70);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:70});
+
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -63,13 +64,15 @@ function draw(){
 
     bird.display();
     platform.display();
-    constraint_log.display();
-    sling.display();
-    
+    //log6.display();
+    slingshot.display();    
 }
+
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
+
+
 function mouseReleased(){
-    sling.fly();
+    slingshot.fly();
 }
